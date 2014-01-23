d86b8f9fb81fa422b4fecbfd50f3c4e367e16d91
Walter Corazzini <waltercorazzini@gmail.com>
2014-01-23 18:18:49 -0300

    c1

---
+++ www/js/functions.js
@@ -0,0 +1,45 @@
+function wide_general() {
+document.getElementById('wide_general').innerHTML = '<ul class="nav-bar vertical">'
++'<li style="background-color:#eee;"><a href="../index.html"><strong>Back</strong></a></li>'
++'<li><a href="#">Article 1</a></li>'
++'<li><a href="#">Article 2</a></li>'
++'<li><a href="#">Article 3</a></li>'
++'</ul>';
+}
+
+
+
+    function topbar_general() {
+    document.getElementById('topbar_general').innerHTML = '<nav class="top-bar">'
+    +'<ul>'
+    +'<li class="name"><h1><a href="#">Category Title</a></h1></li>'
+    +'<li class="toggle-topbar"><a href="#"></a></li>'
+    +'</ul>'
+
+    +'<section>'
+    +'<ul class="left">'
+    +'<li>'
+    +'<a href="../index.html" style="background-color:#eee;">Back</a>'
+    +'</li>'
+    +'</ul>'
+    +'</section>'
+
+    +'<section>'
+    +'<ul class="right">'
+    +'<li><a href="#">Article 1</a>'
+    +'<li><a href="#">Article 2</a></li>'
+    +'<li><a href="#">Article 3</a></li>'
+    +'</ul>'
+    +'</li>'
+    +'</ul>'
+    +'</li>'
+    +'</ul>'
+    +'</section>'
+    +'</nav>';
+    }
+
+
+
+function copyright() {
+document.getElementById('copyright').innerHTML = 'Copyright &copy; 2013 by ';
+}
